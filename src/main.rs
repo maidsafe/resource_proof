813d0ba08c0b84a572952d9bbcbb239300baac1a
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2017-04-05 13:48:13 +0100

    chore/all: update to match standard style and CI settings (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+// Copyright 2017 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under (1) the MaidSafe.net Commercial License,
+// version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+// licence you accepted on initial access to the Software (the "Licences").
+//
+// By contributing code to the SAFE Network Software, or to this project generally, you agree to be
+// bound by the terms of the MaidSafe Contributor Agreement.  This, along with the Licenses can be
+// found in the root directory of this project at LICENSE, COPYING and CONTRIBUTOR.
+//
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+//
+// Please review the Licences for the specific language governing permissions and limitations
+// relating to use of the SAFE Network Software.
+
+//! Command line tool for generating and validating resource proofs.
+
+// For explanation of lint checks, run `rustc -W help` or see
+// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
+#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, improper_ctypes, missing_docs, non_shorthand_field_patterns,
+        overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
+        stable_features, unconditional_recursion, unknown_lints, unsafe_code, unused,
+        unused_allocation, unused_attributes, unused_comparisons, unused_features, unused_parens,
+        while_true)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results)]
+#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
+         missing_debug_implementations, variant_size_differences)]
+
 extern crate resource_proof;
 #[macro_use]
 extern crate clap;
@@ -12,10 +45,9 @@ use std::time::Instant;
 use termion::color;
 
 fn test_it(dif: u8, size: usize, nonce: [u8; 32]) {
-
     let create = Instant::now();
     let rp = ResourceProof::new(size, dif);
-    let ref mut data = rp.create_proof_data(&nonce);
+    let data = &mut rp.create_proof_data(&nonce);
     let proof = rp.create_proof(data);
     let create_time = create.elapsed().as_secs();
     let check = Instant::now();
